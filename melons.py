6603c02275b56a65907f6382c401fcec1cb80b3a
Anastasia Correa <correa.anastasia7@gmail.com>
2020-07-17 19:27:32 +0000

    Altered get_total to include some exceptions

--- melons.py
+++ melons.py
@@ -11,6 +11,10 @@ class AbstractMelonOrder:
     def get_total(self):
 
         base_price = 5
+
+        if self.species == 'Christmas melon':
+            base_price = base_price * 1.5
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -43,3 +47,12 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+
+        total = super().get_total()
+
+        if self.qty < 10:
+            return total + 3
+
+        return total
